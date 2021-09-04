98ba958d499d3a6611e88a3d6cc3565bd09ab577
ScoRoScuba <scott.roberts@virgin.net>
2021-09-04 21:41:13 +1000

    Removed Serilog

--- src/immersed.flight.planning.app/Program.cs
+++ src/immersed.flight.planning.app/Program.cs
@@ -3,7 +3,7 @@ using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Serilog;
+//using Serilog;
 
 namespace immersed.flight.planning.app
 {
@@ -16,9 +16,9 @@ namespace immersed.flight.planning.app
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+/*                .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
                     .ReadFrom.Configuration(hostingContext.Configuration)
-                    .Enrich.FromLogContext())
+                    .Enrich.FromLogContext()) */
                 .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureAppConfiguration((context, builder) =>
                 {
